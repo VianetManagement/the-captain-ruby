2f90ad237b47d803147af961e5bb10a7ee00ab7b
George Karaszi <georgekaraszi@gmail.com>
2017-03-22 16:21:54 -0400

    re-enable basic examples

--- examples/basic.rb
+++ examples/basic.rb
@@ -1,4 +1,4 @@
-require 'dotenv/load'
+require "dotenv/load"
 Dotenv.load("development.env")
 
 require "the_captain"
@@ -20,38 +20,37 @@ puts "Submit for signup:"
 puts JSON.pretty_generate(TheCaptain::IpAddress.submit("206.181.8.242", user_id: user1, event: :user_signup))
 
 # puts "submit for visit"
-# puts JSON.pretty_generate(TheCaptain::IpAddress.submit("206.181.8.242", user_id: user2, event: :visit))
+puts JSON.pretty_generate(TheCaptain::IpAddress.submit("206.181.8.242", user_id: user2, event: :visit))
 #
 puts "Query just IP:"
 puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242"))
 #
-# puts "Query with signup:"
-# puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", event: :user_signup))
+puts "Query with signup:"
+puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", event: :user_signup))
+
+puts "Query with user_id:"
+puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", user_id: user1))
+
+puts "Query with pagination (apply's to all retrieve queries)"
+puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", per: 2, page: 2))
+
+puts "Query pagination alternative (apply's to all retrieve queries)"
+puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", limit: 2, skip: 2))
+
+# If you have a User model, you can submit: user: user
+puts "Submit an email"
+puts JSON.pretty_generate(TheCaptain::EmailAddress.submit("user#{user1}@example.com", user_id: user3, event: :bounce_hard))
+puts JSON.pretty_generate(TheCaptain::EmailAddress.submit("user#{user2}@example.com", user: user3))
 #
-# puts "Query with user_id:"
-# puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", user_id: user1))
+puts "Query email, get lastest usage"
+puts JSON.pretty_generate(TheCaptain::EmailAddress.retrieve("user#{user1}@example.com"))
+puts JSON.pretty_generate(TheCaptain::EmailAddress.retrieve("user#{user2}@example.com"))
 #
-# puts "Query with pagination (apply's to all retrieve queries)"
-# puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", per: 2, page: 2))
-#
-# puts "Query pagination alternative (apply's to all retrieve queries)"
-# puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242", limit: 2, skip: 2))
-#
-# # If you have a User model, you can submit: user: user
-# puts "Submit an email"
-# puts JSON.pretty_generate(TheCaptain::EmailAddress.submit("user#{user1}@example.com", user_id: user3, event: :bounce_hard))
-# puts JSON.pretty_generate(TheCaptain::EmailAddress.submit("user#{user2}@example.com", user: user3))
-#
-# puts "Query email, get lastest usage"
-# puts JSON.pretty_generate(TheCaptain::EmailAddress.retrieve("user#{user1}@example.com"))
-# puts JSON.pretty_generate(TheCaptain::EmailAddress.retrieve("user#{user2}@example.com"))
-#
-# puts "Query email by user"
-# puts JSON.pretty_generate(TheCaptain::EmailAddress.retrieve("user98@example.com", user: user3))
-#
+puts "Query email by user"
+puts JSON.pretty_generate(TheCaptain::EmailAddress.retrieve("user98@example.com", user: user3))
+
 # puts "Submit user data"
-#
-#
+
 # puts JSON.pretty_generate(TheCaptain::User.submit(user3))
 # puts JSON.pretty_generate(TheCaptain::User.submit(user3,
 #                                                   email_address: { value: "baby99@example" },
@@ -62,25 +61,28 @@ puts JSON.pretty_generate(TheCaptain::IpAddress.retrieve("206.181.8.242"))
 #
 # puts JSON.pretty_generate(TheCaptain::User.retrieve(user3))
 #
-#
-# puts "CreditCard submit:"
-# puts JSON.pretty_generate(TheCaptain::CreditCard.submit(cc_fingerprint, ip_address: "206.181.8.242", user_id: user1, event: :success))
-#
-# puts "CreditCard query:"
-# puts JSON.pretty_generate(TheCaptain::CreditCard.retrieve(cc_fingerprint))
-#
-# puts "CreditCard query event:"
-# puts JSON.pretty_generate(TheCaptain::CreditCard.retrieve(cc_fingerprint, event: :success))
-#
-#
-# puts "Content submit:"
-# puts JSON.pretty_generate(TheCaptain::Content.submit("Hello World", user_id: user1, event: :description))
-#
-# puts "Content query:"
-# puts JSON.pretty_generate(TheCaptain::Content.retrieve("Hello World"))
-#
-# puts "Content query event:"
-# puts JSON.pretty_generate(TheCaptain::Content.retrieve("Hello World", event: :description))
-#
-# puts "Content query User:"
-# puts JSON.pretty_generate(TheCaptain::Content.retrieve("Hello World", user_id: user1))
+
+puts "CreditCard submit:"
+puts JSON.pretty_generate(TheCaptain::CreditCard.submit(cc_fingerprint, ip_address: "206.181.8.242", user_id: user1, event: :success))
+
+puts "CreditCard query:"
+puts JSON.pretty_generate(TheCaptain::CreditCard.retrieve(cc_fingerprint))
+
+puts "CreditCard query event:"
+puts JSON.pretty_generate(TheCaptain::CreditCard.retrieve(cc_fingerprint, event: :success))
+
+puts "Content submit:"
+puts JSON.pretty_generate(TheCaptain::Content.submit("Hello World", user_id: user1, event: :description))
+
+
+puts "Content submit:"
+puts JSON.pretty_generate(TheCaptain::Content.submit("Hello World admin@example.com", user_id: user1, event: :description))
+
+puts "Content query:"
+puts JSON.pretty_generate(TheCaptain::Content.retrieve("Hello World"))
+
+puts "Content query event:"
+puts JSON.pretty_generate(TheCaptain::Content.retrieve("Hello World", event: :description))
+
+puts "Content query User:"
+puts JSON.pretty_generate(TheCaptain::Content.retrieve("Hello World", user_id: user1))
