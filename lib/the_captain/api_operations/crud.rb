41cdbe0761bbd5e606645c8d2353223e44853df0
George Karaszi <georgekaraszi@gmail.com>
2017-03-23 12:29:47 -0400

    Add option for accepting array values to be sent

--- lib/the_captain/api_operations/crud.rb
+++ lib/the_captain/api_operations/crud.rb
@@ -8,22 +8,26 @@ module TheCaptain
 
       module ClassMethods
         def request(method:, path:, params: {}, opts: {})
-          response = TheCaptain.request(method, path, params, opts)
-          TheCaptain.last_response = response
+          TheCaptain.last_response = TheCaptain.request(method, path, params, opts)
         end
 
         def retrieve(identifier, options = {})
           raise ArgumentError, "value identifier required" unless identifier
-          request(method: :get, path: api_path, params: { value: identifier }.merge!(options), opts: {})
+          request(method: :get, path: api_path, params: resolve_identifier(identifier).merge!(options), opts: {})
         end
 
         def submit(identifier, event_data = {})
           raise ArgumentError, "value identifier required" unless identifier
-
-          opts = { body: { value: identifier } }
+          opts = { body: resolve_identifier(identifier) }
           opts[:body].merge!(event_data) unless event_data.blank?
 
           request(method: :post, path: api_path, opts: opts)
+        end
+
+        private
+
+        def resolve_identifier(identifier)
+          identifier.kind_of?(Array) ? { values: identifier } : { value: identifier }
         end
       end
     end
