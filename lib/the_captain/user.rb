a377dc8c8d115fe656ce1692ac35fd2a2a8840a8
George Karaszi <georgekaraszi@gmail.com>
2018-08-03 10:27:31 -0400

    fix user's related content path

--- lib/the_captain/user.rb
+++ lib/the_captain/user.rb
@@ -4,7 +4,7 @@ module TheCaptain
   class User < APIResource
     api_paths base:            "/users/%<resource_id>s",
               payments:        "/users/%<resource_id>s/related/payments",
-              content:         "/users/%<resource_id>s/related/payments",
+              content:         "/users/%<resource_id>s/related/content",
               ip_addresses:    "/users/%<resource_id>s/related/ip_addresses",
               credit_cards:    "/users/%<resource_id>s/related/credit_cards",
               email_addresses: "/users/%<resource_id>s/related/email_addresses"
